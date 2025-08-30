165d45cde2aa37006180e4805f0aa5958006d3f3
Alexis Segurel <alexs71400@gmail.com>
2025-08-30 18:56:02 +0200

    second spider getting dates and hours and link to resa

--- yeu/yeu/spiders/yc_boat_spider.py
+++ yeu/yeu/spiders/yc_boat_spider.py
@@ -8,4 +8,31 @@ class YCBoatSpider(scrapy.Spider):
 
     def parse(self, response):
         title = response.css('title::text').extract()
-        yield {'title_debug' : title}
+        
+
+        sem_line = response.css("li.ligne.mois.semaine")
+        sam_line = response.css("li.ligne.mois.weekend.samedi")
+        dim_line = response.css("li.ligne.mois.weekend.dimanche")
+
+        sem_date = sem_line.css("p.date::text").extract()
+        sam_date = sam_line.css("p.date::text").extract()
+        dim_date = dim_line.css("p.date::text").extract()
+
+        sem_line_horaire = sem_line.css("a.horaire")
+        sam_line_horaire = sam_line.css("a.horaire")
+        dim_line_horaire = dim_line.css("a.horaire")
+
+        sem_line_horaire_link = sem_line_horaire.xpath("@href").extract()
+        sam_line_horaire_link = sam_line_horaire.xpath("@href").extract()
+        dim_line_horaire_link = dim_line_horaire.xpath("@href").extract()
+
+        yield {
+            'title_debug' : title,
+            'sem_date': sem_date,
+            'sem_line_horaire': sem_line_horaire_link,
+            'sam_date': sam_date,
+            'sam_line_horaire': sam_line_horaire_link,
+            'dim_date': dim_date,
+            'dim_line_horaire': dim_line_horaire_link
+               }
+
